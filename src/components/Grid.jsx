949bbf8b6c5f2345ec328f14c416ca78d532d5a2
printMelen <alvaro.redondofgb@gmail.com>
2024-02-24 21:37:30 +0100

    Animacion de carga sin letras

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -9,14 +9,18 @@ const Grid = () => {
       .fromTo('.border-right', { height: '0%' }, { height: '100%', duration: 1 }, '-=0.5')
       .fromTo('.border-bottom', { width: '0%' }, { width: '100%', duration: 1 }, '-=0.5')
       .fromTo('.border-left', { height: '0%' }, { height: '100%', duration: 1 }, '-=0.5');
+    tl.to('.primero', { backgroundColor: '#E0E5E7', duration: 1 }, '-=0.25');
+    tl.to('.cuarto', { backgroundColor: '#E0E5E7', duration: 1 }, '-=0.25');
+    tl.to('.segundo', { backgroundColor: '#F0CE06', duration: 1 }, '-=0.25');
+    tl.to('.tercero', { backgroundColor: '#D4121A', duration: 1 }, '-=0.25');
   }, []);
 
   return (
     <div className="grid grid-cols-5 grid-rows-7 h-screen">
       <div className="primero col-span-2 row-span-3 border-black "></div>
-      <div className="my-element col-span-3 row-span-4 col-start-3 border-right border-black border-l-4"></div>
-      <div className="my-element col-span-2 row-span-4 row-start-4 border-top border-black border-t-4"></div>
-      <div className="my-element col-span-3 row-span-3 col-start-3 row-start-5 bg-[#E0E5E7] border-black"></div>
+      <div className="segundo my-element col-span-3 row-span-4 col-start-3 border-right border-black border-l-4"></div>
+      <div className="tercero my-element col-span-2 row-span-4 row-start-4 border-top border-black border-t-4"></div>
+      <div className="cuarto my-element col-span-3 row-span-3 col-start-3 row-start-5 border-left border-top border-t-4 border-l-4 border-black"></div>
     </div>
   );
 }
