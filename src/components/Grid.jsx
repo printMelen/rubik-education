5d98cafbb12a9a9730f2a61bcf4a5961707dcac7
printMelen <alvaro.redondofgb@gmail.com>
2024-02-24 23:02:57 +0100

    Añadiendo fuentes y textos

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -17,10 +17,18 @@ const Grid = () => {
 
   return (
     <div className="grid grid-cols-5 grid-rows-7 h-screen">
-      <div className="primero col-span-2 row-span-3 border-black "></div>
-      <div className="segundo my-element col-span-3 row-span-4 col-start-3 border-right border-black border-l-4"></div>
-      <div className="tercero my-element col-span-2 row-span-4 row-start-4 border-top border-black border-t-4"></div>
-      <div className="cuarto my-element col-span-3 row-span-3 col-start-3 row-start-5 border-left border-top border-t-4 border-l-4 border-black"></div>
+      <div className="primero col-span-2 row-span-3 border-black flex items-center justify-center">
+        <img src="public\icon.svg" alt="" srcset="" />
+      </div>
+      <div className="segundo my-element col-span-3 row-span-4 col-start-3 border-right border-black border-l-4 flex items-center justify-center">
+        <h2 className='text-7xl font-titillium-black font-black uppercase text-white'>Playground</h2>
+      </div>
+      <div className="tercero my-element col-span-2 row-span-4 row-start-4 border-top border-black border-t-4 flex items-center justify-center">
+        <h2 className='text-7xl font-titillium-black font-black uppercase text-white'>Ranking</h2>
+      </div>
+      <div className="cuarto my-element col-span-3 row-span-3 col-start-3 row-start-5 border-left border-top border-t-4 border-l-4 border-black flex items-center justify-center">
+        <h2 className='text-7xl font-titillium-black font-black uppercase text-black'>Account</h2>
+      </div>
     </div>
   );
 }
